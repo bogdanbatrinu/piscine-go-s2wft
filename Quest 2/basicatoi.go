38f81c59f9363c50576aa63032c0c04656ec117a
bogdanbatrinu <reallilsterling@gmail.com>
2025-05-08 10:46:50 +0100

    Update basicatoi.go
    Write a function that simulates the behaviour of the Atoi function in Go. Atoi transforms a number defined as a string into a number defined as an int.
    
    Atoi returns 0 if the string is not considered as a valid number. For this exercise only valid string will be tested. They will only contain one or several digits as characters.
    
    For this exercise the handling of the signs + or - does not have to be taken into account.
    
    This function will only have to return the int. For this exercise the error return of Atoi is not required.

--- Quest 2/basicatoi.go
+++ Quest 2/basicatoi.go
@@ -3,7 +3,7 @@ package main
 func BasicAtoi (s string) int {
   result :=0
   for _, r := range s {
-    result := result*10 + int(r-'0')
+    result = result*10 + int(r-'0')
     }
   return result
   }
